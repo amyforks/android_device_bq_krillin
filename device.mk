ddb881ead9b4c861afc1e16b538646aa004ef9df
JBBgameich <jbbgameich@gmail.com>
2017-11-01 12:50:42 +0100

    Remove some things that are not useful for us from device.mk
    Change-Id: I3e799d2da717d6e00a56348781b8b4769591a8a9

--- device.mk
+++ device.mk
@@ -16,7 +16,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk) 
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 # Call krillin vendor
 $(call inherit-product, vendor/bq/krillin/krillin-vendor.mk)
@@ -71,8 +70,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/etc/apns-conf.xml:system/etc/apns-conf.xml \
     $(LOCAL_PATH)/configs/etc/spn-conf.xml:system/etc/spn-conf.xml \
     $(LOCAL_PATH)/configs/etc/fmr/mt6627_fm_cust.cfg:system/etc/fmr/mt6627_fm_cust.cfg
-	
-PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
@@ -93,10 +90,6 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS := device/bq/krillin/overlay
 
-# Gestures Package for dt2wake and Sw2wake
-PRODUCT_PACKAGES += \
-    Gestures
-
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -106,10 +99,10 @@ PRODUCT_PACKAGES += \
     tinymix
 
 # Mediatek Wlan Packages
-PRODUCT_PACKAGES += \
-    libwpa_client \
-    dhcpcd.conf
-    #wpa_supplicant \
+#PRODUCT_PACKAGES += \
+    libwpa_client¸\
+    dhcpcd.conf \
+    wpa_supplicant \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
@@ -130,22 +123,6 @@ PRODUCT_PACKAGES += \
     libnl_2 \
     libtinyxml
 
-# Filesystem Managment Tools
-PRODUCT_PACKAGES += \
-    setup_fs \
-    e2fsck \
-    fsck.f2fs \
-    mkfs.f2fs \
-    make_ext4fs
-PRODUCT_PACKAGES += \
-    mount.exfat \
-    fsck.exfat \
-    mkfs.exfat
-PRODUCT_PACKAGES += \
-    fsck.ntfs \
-    mkfs.ntfs \
-    mount.ntfs
-
 # GPS package
 PRODUCT_PACKAGES += \
     gps.mt6582
@@ -157,14 +134,6 @@ PRODUCT_PACKAGES += \
 # EGL Necessary package
 PRODUCT_PACKAGES += \
     libstlport
-  
-# Radio FM
-PRODUCT_PACKAGES += \
-    FMRadio
-
-# Snap Package
-PRODUCT_PACKAGES += \
-    Snap
 
 # Default Propierties
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
@@ -173,15 +142,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	ro.allow.mock.location=0 \
 	ro.zygote=zygote32 \
 	camera.disable_zsl_mode=1 \
-	dalvik.vm.dex2oat-Xms=64m \
-	dalvik.vm.dex2oat-Xmx=512m \
-	dalvik.vm.image-dex2oat-Xms=64m \
-	dalvik.vm.image-dex2oat-Xmx=64m \
-	ro.dalvik.vm.native.bridge=0 \
 	ro.debuggable=1 \
 	ro.hardware=sprout \
 	ro.telephony.ril_class=SproutRIL \
-	ro.telephony.ril.config=fakeiccid 
+	ro.telephony.ril.config=fakeiccid
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     pm.dexopt.first-boot=verify-at-runtime \
@@ -193,10 +157,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     pm.dexopt.shared-apk=speed \
     pm.dexopt.forced-dexopt=speed \
     pm.dexopt.core-app=speed
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-filter=speed \
-    dalvik.vm.dex2oat-swap=false	
 
 # Granular Volume Steps
 PRODUCT_PROPERTY_OVERRIDES += \
